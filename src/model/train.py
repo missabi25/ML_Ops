174e86b1361e5db18416efc8750589fa9557aa00
missabi25 <109311922+missabi25@users.noreply.github.com>
2025-01-08 12:36:40 +0530

    Update train.py
    Added AutoLogging & split_data function

--- src/model/train.py
+++ src/model/train.py
@@ -3,25 +3,22 @@
 import argparse
 import glob
 import os
+import mlflow
 
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
-
-    # read data
-    df = get_csvs_df(args.training_data)
-
-    # split data
-    X_train, X_test, y_train, y_test = split_data(df)
-
-    # train model
-    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
+        df = get_csvs_df(args.training_data)
+        X_train, X_test, y_train, y_test = split_data(df)
+        train_model(args.reg_rate, X_train, X_test, y_train, y_test)
 
 
 def get_csvs_df(path):
@@ -34,6 +31,13 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
+def split_data(df, test_size=0.2, random_state=42):
+    X = df.iloc[:, :-1]  # All columns except the last one
+    y = df.iloc[:, -1]   # The last column as the target
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    return X_train, X_test, y_train, y_test
+    
 
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
